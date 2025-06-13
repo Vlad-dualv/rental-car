57f5c08fda86754a7337238432c28b96e4d6c17c
Vlad <vladswork95@gmail.com>
2025-06-13 11:25:29 +0100

    header layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import { NavLink } from "react-router-dom";
-//import css from "./Header.module.css";
+import css from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/catalog">Catalog</NavLink>
-      </nav>
+    <header className={css.header}>
+      <div className={css.headerContainer}>
+        <p className={css.logo}>
+          Rental<span className={css.accentColor}>Car</span>
+        </p>
+        <nav className={css.nav}>
+          <NavLink to="/" className={css.headerLink}>
+            Home
+          </NavLink>
+          <NavLink to="/catalog" className={css.headerLink}>
+            Catalog
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
