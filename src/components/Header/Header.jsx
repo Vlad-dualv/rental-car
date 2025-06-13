c72a92a7b08e676fef2283623c88998c32450542
Vlad <vladswork95@gmail.com>
2025-06-13 15:33:03 +0100

    Header linkstyle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import { NavLink } from "react-router-dom";
 import css from "./Header.module.css";
+import clsx from "clsx";
 
 export default function Header() {
+  const linkStyle = ({ isActive }) => {
+    return clsx(css.nav, isActive && css.active);
+  };
+
   return (
     <header className={css.header}>
       <div className={css.headerContainer}>
@@ -9,10 +14,10 @@ export default function Header() {
           Rental<span className={css.accentColor}>Car</span>
         </p>
         <nav className={css.nav}>
-          <NavLink to="/" className={css.headerLink}>
+          <NavLink to="/" className={linkStyle}>
             Home
           </NavLink>
-          <NavLink to="/catalog" className={css.headerLink}>
+          <NavLink to="/catalog" className={linkStyle}>
             Catalog
           </NavLink>
         </nav>
