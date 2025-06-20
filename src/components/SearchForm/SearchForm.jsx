23b84c2a4fe513dfec77e17d0f6c79391a131ebe
Vlad <vladswork95@gmail.com>
2025-06-20 11:44:29 +0100

    feat: search form layour ready

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,15 +1,19 @@
 import { Formik, Field, Form } from "formik";
 import css from "./SearchForm.module.css";
-import { useId } from "react";
+import { useId, useState } from "react";
 import clsx from "clsx";
 
 export default function SearchForm() {
   const iconArrowDown = "/sprite.svg#icon-arrowdown";
   const iconArrowUp = "/sprite.svg#icon-arrowup";
+
   const brandFieldId = useId();
   const priceFieldId = useId();
   const mileageFieldFromId = useId();
   const mileageFieldToId = useId();
+
+  const [focusedSelect, setFocusedSelect] = useState(null);
+
   return (
     <Formik
       initialValues={{
@@ -30,13 +34,17 @@ export default function SearchForm() {
               name="brand"
               id={brandFieldId}
               className={clsx(css.searchInput, css.brandInput)}
+              onFocus={(e) => setFocusedSelect(e.target.name)}
+              onBlur={() => setFocusedSelect(null)}
             >
               <option value="" disabled selected hidden>
                 Choose brand
               </option>
             </Field>
             <svg aria-label="dropdown icon" className={css.selectIcon}>
-              <use href={iconArrowDown}></use>
+              <use
+                href={focusedSelect === "brand" ? iconArrowUp : iconArrowDown}
+              ></use>
             </svg>
           </div>
         </div>
@@ -50,6 +58,8 @@ export default function SearchForm() {
               name="price"
               id={priceFieldId}
               className={clsx(css.searchInput, css.priceInput)}
+              onFocus={(e) => setFocusedSelect(e.target.name)}
+              onBlur={() => setFocusedSelect(null)}
             >
               <option
                 value=""
@@ -60,9 +70,24 @@ export default function SearchForm() {
               >
                 Choose a price
               </option>
+              <option value="30">$30</option>
+              <option value="40">$40</option>
+              <option value="50">$50</option>
+              <option value="60">$60</option>
+              <option value="70">$70</option>
+              <option value="80">$80</option>
+              <option value="90">$90</option>
+              <option value="100">$100</option>
+              <option value="110">$110</option>
+              <option value="120">$120</option>
+              <option value="130">$130</option>
+              <option value="140">$140</option>
+              <option value="150">$150</option>
             </Field>
             <svg aria-label="dropdown icon" className={css.selectIcon}>
-              <use href={iconArrowDown}></use>
+              <use
+                href={focusedSelect === "price" ? iconArrowUp : iconArrowDown}
+              ></use>
             </svg>
           </div>
         </div>
