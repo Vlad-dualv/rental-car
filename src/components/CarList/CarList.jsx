ac75e8fbc564e864511cb8ab792ce83f1947114c
Vlad <vladswork95@gmail.com>
2025-06-22 10:48:22 +0100

    fix: sorry message and load more button logic

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,39 +1,18 @@
 import css from "./CarList.module.css";
-import { useSelector } from "react-redux";
 import CarItem from "../CarItem/CarItem";
-import { selectCars } from "../../redux/cars/slice";
-import {
-  selectBrand,
-  selectPrice,
-  selectMileage,
-} from "../../redux/filters/selectors";
 
-export default function CarList() {
-  const cars = useSelector(selectCars);
-  const brand = useSelector(selectBrand);
-  const price = useSelector(selectPrice);
-  const mileage = useSelector(selectMileage);
-
-  const filteredCars = cars.filter((car) => {
-    const matchesBrand = brand ? car.brand === brand : true;
-    const matchesPrice = price
-      ? Number(car.rentalPrice.replace("$", "")) <= Number(price)
-      : true;
-    const matchesMileage = mileage ? car.mileage <= Number(mileage) : true;
-    return matchesBrand && matchesPrice && matchesMileage;
-  });
-
-  if (filteredCars.length === 0) {
+export default function CarList({ cars, hasFetched }) {
+  if (hasFetched && cars.length === 0) {
     return (
-      <p className={css.noCarsMessage}>
+      <h3 className={css.noCarsMessage}>
         Sorry, we don't have cars with these parameters.
-      </p>
+      </h3>
     );
   }
 
   return (
     <ul className={css.carList}>
-      {filteredCars.map((car) => (
+      {cars.map((car) => (
         <li key={car.id}>
           <CarItem car={car} />
         </li>
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -7,6 +7,12 @@ import {
   selectPage,
   selectTotalPages,
 } from "../../redux/cars/slice";
+import { selectCars } from "../../redux/cars/slice";
+import {
+  selectBrand,
+  selectPrice,
+  selectMileage,
+} from "../../redux/filters/selectors";
 import CarList from "../../components/CarList/CarList";
 import css from "./Catalog.module.css";
 import Loader from "../../components/Loader/Loader";
@@ -18,6 +24,22 @@ export default function Catalog() {
   const isLoading = useSelector(selectLoading);
   const page = useSelector(selectPage);
   const totalPages = useSelector(selectTotalPages);
+
+  const cars = useSelector(selectCars);
+  const brand = useSelector(selectBrand);
+  const price = useSelector(selectPrice);
+  const mileage = useSelector(selectMileage);
+
+  const filteredCars = cars.filter((car) => {
+    const matchesBrand = brand ? car.brand === brand : true;
+    const matchesPrice = price
+      ? Number(car.rentalPrice.replace("$", "")) <= Number(price)
+      : true;
+    const matchesMileage = mileage ? car.mileage <= Number(mileage) : true;
+    return matchesBrand && matchesPrice && matchesMileage;
+  });
+
+  const hasFethced = !isLoading && (cars.length > 0 || page > 1);
 
   useEffect(() => {
     dispatch(fetchCars(1));
@@ -31,8 +53,8 @@ export default function Catalog() {
     <main>
       <section className={css.catalogSection}>
         <SearchForm />
-        <CarList />
-        {page < totalPages && (
+        <CarList cars={filteredCars} hasFetched={hasFethced} />
+        {filteredCars.length > 0 && page < totalPages && (
           <button
             type="button"
             className={css.loadButton}
